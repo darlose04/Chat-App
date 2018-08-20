8a9d06989fef8e45577ea461a75d3b3a9c5d3670
Zach Smith <zsmith126028@gmail.com>
2018-08-20 14:28:05 -0500

    move routes to routes directory

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var express = require("express"),
   mongoose = require("mongoose"),
   passport = require("passport"),
   LocalStrategy = require("passport-local"),
-  Chat = require("./models/chat"),
-  Comment = require("./models/comment"),
+  // Chat = require("./models/chat"),
+  // Comment = require("./models/comment"),
   User = require("./models/user");
+
+// requiring the routes
+var chatRoutes = require("./routes/chats"),
+    commentRoutes = require("./routes/comments"),
+    indexRoutes = require("./routes/index");
   
 mongoose.connect("mongodb://localhost:27017/chat_app", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,146 +35,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get("/", function(req, res) {
-  res.render("landing");
-});
-
-// INDEX - show chats
-app.get("/chats", function(req, res) {
-  // get the chats from the database
-  Chat.find({}, function(err, allChats) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("chats/index", { chats: allChats });
-    }
-  });
-});
-
-// CREATE - add a new chat to the database
-app.post("/chats", function(req, res) {
-  // get data from form and add to chats array
-  var name = req.body.name;
-  var desc = req.body.description;
-  var newChat = { name: name, description: desc };
-  // create new chat and save to the DB
-  Chat.create(newChat, function(err, newlyCreated) {
-    if (err) {
-      console.log(err);
-    } else {
-      // redirect back to chats page
-      res.redirect("/chats");
-    }
-  });
-});
-
-// NEW - show form to create a new chat
-app.get("/chats/new", function(req, res) {
-  res.render("chats/new");
-});
-
-// SHOW - shows more info about one chat
-app.get("/chats/:id", function(req, res) {
-  // find the chat with the provided ID
-  Chat.findById(req.params.id)
-    .populate("comments")
-    .exec(function(err, foundChat) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(foundChat);
-        // render show template with that chat
-        res.render("chats/show", { chat: foundChat });
-      }
-    });
-});
-
-// ============
-// COMMENTS ROUTES
-// ============
-
-app.get("/chats/:id/comments/new", isLoggedIn, function(req, res) {
-  // find chat by ID
-  Chat.findById(req.params.id, function(err, chat) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("comments/new", { chat: chat });
-    }
-  });
-});
-
-app.post("/chats/:id/comments", isLoggedIn, function(req, res) {
-  // look up chat using the id
-  Chat.findById(req.params.id, function(err, chat) {
-    if (err) {
-      console.log(err);
-      res.redirect("/chats");
-    } else {
-      console.log(req.body.comment);
-      // create new comment
-      Comment.create(req.body.comment, function(err, comment) {
-        if (err) {
-          console.log(err);
-        } else {
-          // pushes comment into database and saves it
-          // then redirects back to the chat page
-          chat.comments.push(comment);
-          chat.save();
-          res.redirect("/chats/" + chat._id);
-        }
-      });
-    }
-  });
-});
-
-// AUTHENTICATION ROUTES
-
-// show register form
-app.get("/register", function(req, res){
-  res.render("register");
-});
-
-// handle sign up logic
-app.post("/register", function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err) {
-      console.log(err);
-      return res.render("register");
-    }
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/chats");
-    });
-  });
-});
-
-// show login form
-app.get("/login", function(req, res){
-  res.render("login");
-});
-
-// handles login logic // will have to use middleware here
-app.post("/login", passport.authenticate("local",
-  {
-    successRedirect: "/chats",
-    failureRedirect: "/login"
-  }), function(req, res) {
-});
-
-// logout route
-app.get("/logout", function(req, res){
-  req.logout();
-  res.redirect("/chats");
-});
-
-// middleware
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
-}
+app.use("/", indexRoutes);
+app.use("/chats", chatRoutes);
+app.use("/chats/:id/comments", commentRoutes);
 
 app.listen(2000, function() {
   console.log("Chat app has started!");
