a0c91899a33ef3ab448c6cfadb970694f490e7cd
Zach Smith <zsmith126028@gmail.com>
2018-08-20 14:27:43 -0500

    add routes directory

---
+++ routes/chats.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var router = express.Router();
+var Chat = require("../models/chat");
+
+// INDEX - show chats
+router.get("/chats", function (req, res) {
+    // get the chats from the database
+    Chat.find({}, function (err, allChats) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("chats/index", { chats: allChats });
+        }
+    });
+});
+
+// CREATE - add a new chat to the database
+router.post("/chats", function (req, res) {
+    // get data from form and add to chats array
+    var name = req.body.name;
+    var desc = req.body.description;
+    var newChat = { name: name, description: desc };
+    // create new chat and save to the DB
+    Chat.create(newChat, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            // redirect back to chats page
+            res.redirect("/chats");
+        }
+    });
+});
+
+// NEW - show form to create a new chat
+router.get("/chats/new", function (req, res) {
+    res.render("chats/new");
+});
+
+// SHOW - shows more info about one chat
+router.get("/chats/:id", function (req, res) {
+    // find the chat with the provided ID
+    Chat.findById(req.params.id)
+        .populate("comments")
+        .exec(function (err, foundChat) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(foundChat);
+                // render show template with that chat
+                res.render("chats/show", { chat: foundChat });
+            }
+        });
+});
+
+module.exports = router;
---
+++ routes/comments.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Chat = require("../models/chat");
+var Comment = require("../models/comment");
+
+// ============
+// COMMENTS ROUTES
+// ============
+
+router.get("/chats/:id/comments/new", isLoggedIn, function (req, res) {
+    // find chat by ID
+    Chat.findById(req.params.id, function (err, chat) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { chat: chat });
+        }
+    });
+});
+
+router.post("/chats/:id/comments", isLoggedIn, function (req, res) {
+    // look up chat using the id
+    Chat.findById(req.params.id, function (err, chat) {
+        if (err) {
+            console.log(err);
+            res.redirect("/chats");
+        } else {
+            console.log(req.body.comment);
+            // create new comment
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    // pushes comment into database and saves it
+                    // then redirects back to the chat page
+                    chat.comments.push(comment);
+                    chat.save();
+                    res.redirect("/chats/" + chat._id);
+                }
+            });
+        }
+    });
+});
+
+// middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
