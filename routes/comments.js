a2c854a7760063694b363b539cda4b011812f290
Zach Smith <zsmith126028@gmail.com>
2018-08-20 17:10:03 -0500

    add edit, update, delete routes. add middleware

--- routes/comments.js
+++ routes/comments.js
@@ -1,60 +1,86 @@
 var express = require("express");
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 var Chat = require("../models/chat");
 var Comment = require("../models/comment");
+var middleWare = require("../middleware");
 
 // ============
 // COMMENTS ROUTES
 // ============
 
 // new comments
-router.get("/new", isLoggedIn, function (req, res) {
-    // find chat by ID
-    Chat.findById(req.params.id, function (err, chat) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", { chat: chat });
-        }
-    });
+router.get("/new", middleWare.isLoggedIn, function(req, res) {
+  // find chat by ID
+  Chat.findById(req.params.id, function(err, chat) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", { chat: chat });
+    }
+  });
 });
 
 // create comments
-router.post("/", isLoggedIn, function (req, res) {
-    // look up chat using the id
-    Chat.findById(req.params.id, function (err, chat) {
+router.post("/", middleWare.isLoggedIn, function(req, res) {
+  // look up chat using the id
+  Chat.findById(req.params.id, function(err, chat) {
+    if (err) {
+      console.log(err);
+      res.redirect("/chats");
+    } else {
+      console.log(req.body.comment);
+      // create new comment
+      Comment.create(req.body.comment, function(err, comment) {
         if (err) {
-            console.log(err);
-            res.redirect("/chats");
+          console.log(err);
         } else {
-            console.log(req.body.comment);
-            // create new comment
-            Comment.create(req.body.comment, function (err, comment) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    // add username and id to comment
-                    comment.author.id = req.user._id;
-                    comment.author.username = req.user.username;
-                    // save comment
-                    comment.save();
-                    // pushes comment into database and saves it
-                    // then redirects back to the chat page
-                    chat.comments.push(comment);
-                    chat.save();
-                    res.redirect("/chats/" + chat._id);
-                }
-            });
+          // add username and id to comment
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          // save comment
+          comment.save();
+          // pushes comment into database and saves it
+          // then redirects back to the chat page
+          chat.comments.push(comment);
+          chat.save();
+          res.redirect("/chats/" + chat._id);
+        }
+      });
+    }
+  });
+});
+
+// EDIT comments
+router.get("/:comment_id/edit", middleWare.checkCommentOwnership, function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {chat_id: req.params.id, comment: foundComment});
         }
     });
 });
 
-// middleware
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
+// UPDATE comments
+router.put("/:comment_id", middleWare.checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/chats/" + req.params.id);
+        }
+    });
+});
+
+// DESTROY comments
+router.delete("/:comment_id", middleWare.checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/chats/" + req.params.id);
+        }
+    });
+});
 
 module.exports = router;
