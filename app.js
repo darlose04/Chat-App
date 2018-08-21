17348a84e7921e3ff3fb852cb430460e390a2d83
Zach Smith <zsmith126028@gmail.com>
2018-08-20 21:35:10 -0500

    add db link

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ var express = require("express"),
   // Comment = require("./models/comment"),
   User = require("./models/user");
 
+var PORT = process.env.PORT || 2000;
+
 // requiring the routes
 var chatRoutes = require("./routes/chats"),
-    commentRoutes = require("./routes/comments"),
-    indexRoutes = require("./routes/index");
-  
-mongoose.connect("mongodb://localhost:27017/chat_app", { useNewUrlParser: true });
+  commentRoutes = require("./routes/comments"),
+  indexRoutes = require("./routes/index");
+
+var url = process.env.CHATAPPDBURL || "mongodb://localhost:27017/chat_app";
+
+mongoose.connect(url, {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -23,11 +28,13 @@ app.use(methodOverride("_method"));
 app.use(flash());
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-  secret: "Shadow is the best dog ever",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Shadow is the best dog ever",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -36,7 +43,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
-  res.locals.error=req.flash("error");
+  res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
   next();
 });
@@ -45,7 +52,6 @@ app.use("/", indexRoutes);
 app.use("/chats", chatRoutes);
 app.use("/chats/:id/comments", commentRoutes);
 
-app.listen(2000, function() {
+app.listen(PORT, function() {
   console.log("Chat app has started!");
 });
-
