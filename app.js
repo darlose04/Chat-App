c8550295481c2a7ebf18dc75d1759fa35c2c8a4d
Zach Smith <zsmith126028@gmail.com>
2018-08-20 22:35:26 -0500

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,11 @@ var chatRoutes = require("./routes/chats"),
   commentRoutes = require("./routes/comments"),
   indexRoutes = require("./routes/index");
 
-var url = process.env.CHATAPPURL || "mongodb://localhost:27017/chat_app";
-mongoose.connect(url, {useNewUrlParser: true});
+console.log(process.env.DBURL);
+mongoose.connect(process.env.DBURL);
+
+// var url = process.env.DBURL || "mongodb://localhost:27017/chat_app";
+// mongoose.connect(url,{ useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
