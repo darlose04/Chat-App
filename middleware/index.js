e6f78449c11c51f4f1d853c8630b637c809edb9b
Zach Smith <zsmith126028@gmail.com>
2018-08-20 20:15:44 -0500

    add flash alerts to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -9,18 +9,21 @@ middlewareObj.checkChatOwnership = function(req, res, next) {
     // is user logged in
     if (req.isAuthenticated()) {
         Chat.findById(req.params.id, function(err, foundChat) {
-            if(err) {
+            if(err || !foundChat) {
+                req.flash("error", "Chat not found");
                 res.redirect("back");
             } else {
                 // does user own campground?
                 if (foundChat.author.id.equals(req.user._id)) { // use .equals: first id is mongoose id, second id is string id: == or === won't work
                     next();
                 } else {
+                    req.flash("error", "You do not have permission to do that");
                     res.redirect("back");
                 }
             }
         });
     } else {
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 }
@@ -29,18 +32,21 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
     // is user logged in
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, function(err, foundComment) {
-            if (err) {
+            if (err || !foundComment) {
+                req.flash("error", "Comment not found");
                 res.redirect("back");
             } else {
                 // does user own comment?
                  if (foundComment.author.id.equals(req.user._id)) {
                      next();
                  } else {
+                     req.flash("error", " You do not have permission to do that");
                      res.redirect("back");
                  }
             }
         });
     } else {
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 }
@@ -49,6 +55,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash("error", "You need to be logged in to do that");
     res.redirect("/login");
 }
 
